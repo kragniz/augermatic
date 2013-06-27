4644b93afd13f592e0eeeb98d3949b3060c50e52
Louis Taylor <kragniz@gmail.com>
2013-06-27 16:17:29 +0100

    Started adding form items from dict

--- form.py
+++ form.py
@@ -4,15 +4,21 @@ from gi.repository import Gtk
 class Form(Gtk.Box):
     def __init__(self, items):
         Gtk.Box.__init__(self, spacing=6, orientation=Gtk.Orientation.VERTICAL)
-        self.button = Gtk.Button(label='Click this')
-        self.pack_start(self.button, True, True, 6)
+        self.fields = []
+        for item in items:
+            box = Gtk.Box(spacing=6)
+            label = Gtk.Label(item['id'])
+            self.fields += [Gtk.Button(label=item['label'])]
+            box.pack_start(label, False, True, 6)
+            box.pack_start(self.fields[-1], True, True, 6)
+            self.pack_start(box, True, True, 6)
 
 if __name__ == '__main__':
     win = Gtk.Window(title='Form test')
     form = Form(
                   [
-                     ['database-no', 'Database Number'],
-                     ['date', 'Date']
+                     {'id': 'database-no', 'label': 'Database Number'},
+                     {'id': 'date', 'label': 'Date'}
                   ]
                )
     win.add(form)
